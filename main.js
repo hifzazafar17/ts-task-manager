a716d6a578e8a4bd7516f3a01719af71da42ef2f
Hifza Zafar <152292315+Hifza-Code-Journey@users.noreply.github.com>
2024-03-22 14:57:49 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+import inquirer from "inquirer";
+const tasks = [];
+let taskId = 1;
+async function main() {
+    while (true) {
+        const { choice } = await inquirer.prompt({
+            type: "list",
+            name: "choice",
+            message: "Choose an option:",
+            choices: ["Add task", "View Task", "Exit",],
+        });
+        if (choice === "Add task") {
+            const { taskName } = await inquirer.prompt({
+                type: "input",
+                name: "taskName",
+                message: "enter the task name:",
+            });
+            tasks.push({
+                id: taskId++,
+                name: taskName,
+                completed: false
+            });
+            console.log("Task Added successfully!");
+        }
+        else if (choice === "View Task") {
+            if (tasks.length === 0) {
+                console.log("No task available.");
+            }
+            else {
+                console.log("Current Tasks:");
+                tasks.forEach((tasks) => {
+                    const status = tasks.completed ? "[Completed]" : "[Pending]";
+                    console.log(`${tasks.id}. ${status} ${tasks.name}`);
+                });
+            }
+        }
+        else if (choice === "Exit") {
+            console.log("exiting task manager.");
+            break;
+        }
+    }
+}
+main();
